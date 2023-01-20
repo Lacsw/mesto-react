9cc88040e27b592146607fdf24f974993dacd163
Roman Frolov <frolloff1@yandex.ru>
2023-01-20 18:28:33 +0300

    ref: опустить пропс children в попапах

--- src/components/App.js
+++ src/components/App.js
@@ -51,39 +51,36 @@ function App() {
         submitBtnText="Сохранить"
         isOpen={isEditProfilePopupOpen}
         onClose={closeAllPopups}
-        children={
-          <>
-            <fieldset className="popup__set">
-              <label className="popup__field">
-                <input
-                  className="popup__input popup__input_type_name"
-                  type="text"
-                  name="name"
-                  placeholder="Укажите Ваше имя"
-                  id="name-input"
-                  minLength="2"
-                  maxLength="40"
-                  required
-                />
-                <span className="popup__input-error name-input-error"></span>
-              </label>
-              <label className="popup__field">
-                <input
-                  className="popup__input popup__input_type_job"
-                  type="text"
-                  name="about"
-                  placeholder="Укажите Вашу профессию"
-                  id="job-input"
-                  minLength="2"
-                  maxLength="200"
-                  required
-                />
-                <span className="popup__input-error job-input-error"></span>
-              </label>
-            </fieldset>
-          </>
-        }
-      />
+      >
+        <fieldset className="popup__set">
+          <label className="popup__field">
+            <input
+              className="popup__input popup__input_type_name"
+              type="text"
+              name="name"
+              placeholder="Укажите Ваше имя"
+              id="name-input"
+              minLength="2"
+              maxLength="40"
+              required
+            />
+            <span className="popup__input-error name-input-error"></span>
+          </label>
+          <label className="popup__field">
+            <input
+              className="popup__input popup__input_type_job"
+              type="text"
+              name="about"
+              placeholder="Укажите Вашу профессию"
+              id="job-input"
+              minLength="2"
+              maxLength="200"
+              required
+            />
+            <span className="popup__input-error job-input-error"></span>
+          </label>
+        </fieldset>
+      </PopupWithForm>
 
       <PopupWithForm
         name="add"
@@ -91,37 +88,34 @@ function App() {
         submitBtnText="Создать"
         isOpen={isAddPlacePopupOpen}
         onClose={closeAllPopups}
-        children={
-          <>
-            <fieldset className="popup__set">
-              <label className="popup__field">
-                <input
-                  className="popup__input popup__input_type_place-name"
-                  type="text"
-                  name="place-name"
-                  placeholder="Название"
-                  minLength="2"
-                  maxLength="30"
-                  id="place-name-input"
-                  required
-                />
-                <span className="popup__input-error place-name-input-error"></span>
-              </label>
-              <label className="popup__field">
-                <input
-                  className="popup__input popup__input_type_link"
-                  type="url"
-                  name="link"
-                  placeholder="Ссылка на картинку"
-                  id="link-input"
-                  required
-                />
-                <span className="popup__input-error link-input-error"></span>
-              </label>
-            </fieldset>
-          </>
-        }
-      />
+      >
+        <fieldset className="popup__set">
+          <label className="popup__field">
+            <input
+              className="popup__input popup__input_type_place-name"
+              type="text"
+              name="place-name"
+              placeholder="Название"
+              minLength="2"
+              maxLength="30"
+              id="place-name-input"
+              required
+            />
+            <span className="popup__input-error place-name-input-error"></span>
+          </label>
+          <label className="popup__field">
+            <input
+              className="popup__input popup__input_type_link"
+              type="url"
+              name="link"
+              placeholder="Ссылка на картинку"
+              id="link-input"
+              required
+            />
+            <span className="popup__input-error link-input-error"></span>
+          </label>
+        </fieldset>
+      </PopupWithForm>
 
       <PopupWithForm
         name="confirm"
@@ -136,22 +130,19 @@ function App() {
         submitBtnText="Сохранить"
         isOpen={isEditAvatarPopupOpen}
         onClose={closeAllPopups}
-        children={
-          <>
-            <label className="popup__field">
-              <input
-                className="popup__input popup__input_type_avatar"
-                type="url"
-                name="avatar"
-                placeholder="Ссылка на новый аватар"
-                id="avatar-link-input"
-                required
-              />
-              <span className="popup__input-error avatar-link-input-error"></span>
-            </label>
-          </>
-        }
-      />
+      >
+        <label className="popup__field">
+          <input
+            className="popup__input popup__input_type_avatar"
+            type="url"
+            name="avatar"
+            placeholder="Ссылка на новый аватар"
+            id="avatar-link-input"
+            required
+          />
+          <span className="popup__input-error avatar-link-input-error"></span>
+        </label>
+      </PopupWithForm>
 
       <ImagePopup
         card={selectedCard}
